78bb63c7d5ad53c4f00aa7a1036bbc0175835739
TenshiMunasinghe <shadowghost1007@gmail.com>
2026-01-10 22:44:17 +0100

    finished padding selector

--- src/components/paddingSelector.svelte
+++ src/components/paddingSelector.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
 	import Button from '$lib/components/ui/button/button.svelte';
-	import { MoveHorizontal, MoveVertical, Scan, Square } from 'lucide-svelte';
+	import {
+		MoveHorizontal,
+		MoveRight,
+		MoveDown,
+		MoveUp,
+		MoveVertical,
+		PanelLeftDashed,
+		PanelTopBottomDashed,
+		SquareDashed,
+		MoveLeft
+	} from 'lucide-svelte';
 	import Slider from '$lib/components/ui/slider/slider.svelte';
 
 	type PaddingMode = 'uniform' | 'axes' | 'individual';
@@ -36,7 +46,7 @@
 				onValueChange={(value) => updateAll(value)}
 			/>
 			<Button onclick={() => (state = 'axes')}>
-				<Square />
+				<SquareDashed />
 			</Button>
 		</div>
 	{:else if state === 'axes'}
@@ -67,13 +77,65 @@
 					/>
 				</div>
 			</div>
-			<Button>
-				<Scan />
+			<Button onclick={() => (state = 'individual')}>
+				<PanelTopBottomDashed />
 			</Button>
 		</div>
 	{:else if state === 'individual'}
-		<div>
-			<h1>Individual</h1>
+		<div class="flex items-center gap-2">
+			<div class="grid grow grid-cols-2 gap-2">
+				<div class="flex items-center gap-2">
+					<MoveUp class="h-4 w-4" />
+					<Slider
+						type="single"
+						min={0}
+						max={64}
+						step={2}
+						value={padding.top}
+						onValueChange={(value) => updateIndividual('top', value)}
+						class="w-full grow"
+					/>
+				</div>
+				<div class="flex items-center gap-2">
+					<MoveRight class="h-4 w-4" />
+					<Slider
+						type="single"
+						min={0}
+						max={64}
+						step={2}
+						value={padding.right}
+						onValueChange={(value) => updateIndividual('right', value)}
+						class="w-full grow"
+					/>
+				</div>
+				<div class="flex items-center gap-2">
+					<MoveDown class="h-4 w-4" />
+					<Slider
+						type="single"
+						min={0}
+						max={64}
+						step={2}
+						value={padding.bottom}
+						onValueChange={(value) => updateIndividual('bottom', value)}
+						class="w-full grow"
+					/>
+				</div>
+				<div class="flex items-center gap-2">
+					<MoveLeft class="h-4 w-4" />
+					<Slider
+						type="single"
+						min={0}
+						max={64}
+						step={2}
+						value={padding.left}
+						onValueChange={(value) => updateIndividual('left', value)}
+						class="w-full grow"
+					/>
+				</div>
+			</div>
+			<Button onclick={() => (state = 'uniform')}>
+				<PanelLeftDashed />
+			</Button>
 		</div>
 	{/if}
 </div>
